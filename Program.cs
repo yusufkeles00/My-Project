c266309c939d79fbd2062150b8ea0aa59a0bff81
yusufkeles00 <68776083+yusufkeles00@users.noreply.github.com>
2020-07-25 18:33:36 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace deneme_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int girilensayi;
+            Random rastgele = new Random();
+            int sayi = rastgele.Next(1, 11);
+            int kalanhak = 3;
+            Console.WriteLine("!!!Sayi Tahmin Programına Hoşgeldiniz!!!");
+            do
+            {
+                Console.Write("Tahmininizi Girin (1-10 arası): ");
+                girilensayi = Convert.ToInt32(Console.ReadLine());
+                if (sayi == girilensayi)
+                {
+                    Console.WriteLine("Doğru Tahmin!!!");
+
+                }
+                else
+                {
+                    kalanhak--;
+                    Console.WriteLine("Yanlış Tahmin Tekrar Deneyin! \n(Kalan Hak: {0})", kalanhak);
+                }
+            } while ((sayi != girilensayi) && (kalanhak > 0));
+            Console.WriteLine("Oyunu Kazanamadınız!");
+            Console.ReadKey();
+        }
+    }
+}
