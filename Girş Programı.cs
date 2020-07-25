4c51d961bc18943c878fc4c1b9a3cbf5b670469f
yusufkeles00 <68776083+yusufkeles00@users.noreply.github.com>
2020-07-25 14:34:15 +0300

    Add files via upload

---
+++ Girş Programı.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Deneme1_1_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string defkullanici = "yusufkeles";
+            int defsifre = 1234;
+            int kalanhak = 3;
+            string kullanici;
+            int sifre;
+            while (kalanhak != 0)
+            {
+                Console.Write("Kullanıcı Adınızı Giriniz: ");
+                kullanici = Console.ReadLine();
+                Console.Write("Şifrenizi Giriniz: ");
+                sifre = Convert.ToInt32(Console.ReadLine());
+                if (defkullanici == kullanici && defsifre == sifre)
+                {
+                    Console.WriteLine("Giriş Başarılı! Hoşgeldin {0}", defkullanici);
+                    break;
+                }
+                else
+                {
+                    kalanhak--;
+                    Console.WriteLine("Şifre Yanlış!!! Kalan Deneme Hakkı: {0}", kalanhak);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
